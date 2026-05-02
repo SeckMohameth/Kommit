d2440e353f20af6c371d0362867d468aadf278fe
SeckMohameth <mohamethseck34@gmail.com>
2026-05-02 18:34:10 -0400

    Initial Commit

---
+++ Kommit/KommitApp.swift
@@ -0,0 +1,17 @@
+//
+//  KommitApp.swift
+//  Kommit
+//
+//  Created by Mohameth Seck on 5/2/26.
+//
+
+import SwiftUI
+
+@main
+struct KommitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
